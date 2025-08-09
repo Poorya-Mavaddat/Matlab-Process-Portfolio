d3a71f50cac75a7c3fc454d50423d3fc6b4568ba
Poorya Mavaddat <poorya.mavaddat@gmail.com>
2025-08-09 20:48:30 +0200

    Delete functions directory

--- functions/energy_model.m
+++
@@ -1,5 +0,0 @@
-function E = energy_model(R)
-% ENERGY_MODEL  Simplified energy (reboiler duty proxy) as a function of reflux ratio R.
-%   E(R) = 0.5*R^2 + 1.0*R + 1.0
-    E = 0.5.*R.^2 + 1.0.*R + 1.0;
-end
